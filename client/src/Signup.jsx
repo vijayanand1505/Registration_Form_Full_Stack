ff119b1258f2744b6f278e0cf25d993e568e588d
Vijay Anand <139949005+vijayanand1505@users.noreply.github.com>
2024-10-15 11:09:40 +0530

    Update Signup.jsx

--- client/src/Signup.jsx
+++ client/src/Signup.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import React from 'react'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom' 
 
 function Signup(){
 
