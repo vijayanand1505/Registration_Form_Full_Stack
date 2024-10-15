ca5265fa4bb8333eaef3edfbe61e225b7a903feb
Vijay Anand <139949005+vijayanand1505@users.noreply.github.com>
2024-10-15 11:07:19 +0530

    Update Home.jsx

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Home(){
   return (
     <div className='home-container d-flex justify-content-center align-items-center flex-column text-center mt-5 pt-5 pb-5 bg-light text-dark rounded-3 shadow-lg' >
-        <h1>Home Component</h1>
+        <h1>Home Component</h1> 
     </div>
   )
 }
